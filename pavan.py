51efec49a5068f6019aab4c1bdaf3021b791c130
Starr Yang <starrtyang@berkeley.edu>
2018-02-17 18:36:04 -0800

    imported math.ceil, used A4piano.wav since the other file isn't in the repo, placed function call under if name is main, stack overflow at the end

--- pavan.py
+++ pavan.py
@@ -1,25 +1,23 @@
 from scipy.io import wavfile
 from scipy.fftpack import fft
 import numpy as np
+from math import ceil
 
 def pavan():
-    sampFreq, snd = wavfile.read('440_sine.wav')
-    #fft_output = fft(snd)
+    sampFreq, snd = wavfile.read('A4piano.wav')
     norm = 2.**(int(str(snd.dtype)[-2:]) - 1) # normalize between -1 and 1
     snd = snd / norm
-    #print(snd.shape, snd)
     s1 = snd[:,0]
     print(s1.shape, s1)
     n = len(s1)
     p = fft(s1)
     nUniquePts = int(ceil((n + 1)/2.0))
     p = abs(p[0:nUniquePts]) #absolute value provides magnitude
+    pav = (lambda s: pav(print('pavan')))
+    pav(print('UwU'))
+    
 
 
 
-pavan()
-#print(norm)
-#a = snd.dtype
-#print(a)
-#print(sampFreq)
-#print(snd)
+if __name__ == '__main__':
+    pavan()
